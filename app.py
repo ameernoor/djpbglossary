2a8cbbf3a1ceace897d8c5a3a8d70c9c27a3cbf6
ameernoor <m.ameer.noor92@gmail.com>
2024-06-04 17:44:38 +0700

    change image extension code

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ df = pd.read_excel(file_path, header=2)
 st.title('Glossary Direktorat Jenderal Perbendaharaan')
 
 # Display logos at the top of the page
-logo1 = os.path.join(BASE_DIR, 'images', 'kemenkeu.png')
-logo2 = os.path.join(BASE_DIR, 'images', 'djpb.png')
-logo3 = os.path.join(BASE_DIR, 'images', 'intress.png')
+logo1 = os.path.join(BASE_DIR, 'images', 'kemenkeu.jpeg')
+logo2 = os.path.join(BASE_DIR, 'images', 'djpb.jpeg')
+logo3 = os.path.join(BASE_DIR, 'images', 'intress.jpeg')
 st.image([logo1, logo2, logo3], width=100)
 
 
