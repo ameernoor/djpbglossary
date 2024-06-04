ee2807737585c26396e2905c1c968ea38849d15a
ameernoor <m.ameer.noor92@gmail.com>
2024-06-04 18:26:10 +0700

    correcting error

--- app.py
+++ app.py
@@ -6,17 +6,16 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(BASE_DIR, "Glossary_Compile.xlsx")
 df = pd.read_excel(file_path, header=2)
 
-st.title('Glossary Direktorat Jenderal Perbendaharaan')
-
 # Display logos at the top of the page
 logo1 = os.path.join(BASE_DIR, 'images', 'kemenkeu.jpeg')
 logo2 = os.path.join(BASE_DIR, 'images', 'djpb.jpeg')
 logo3 = os.path.join(BASE_DIR, 'images', 'intress.jpeg')
 st.image([logo1, logo2, logo3], width=100)
 
+st.title('Glossary Direktorat Jenderal Perbendaharaan')
 
-# Input for search query
-query = st.text_input("Masukan Kata Kunci:")
+## Input for search query with a unique key
+query = st.text_input("Masukan Kata Kunci:", key="main_search")
 
 # Filtering data based on search query
 if query:
