2eb5d00761770ff2c5d0c6b5f6c59b80324361b9
ameernoor <m.ameer.noor92@gmail.com>
2024-06-07 15:16:30 +0700

    testing minor change

--- app.py
+++ app.py
@@ -13,22 +13,22 @@ logo1 = os.path.join(BASE_DIR, 'images', 'kemenkeu.png')
 logo2 = os.path.join(BASE_DIR, 'images', 'djpb.png')
 logo3 = os.path.join(BASE_DIR, 'images', 'intress.png')
 
-# Create columns for the logos
-col1, col2, col3 = st.columns(3)
+# Create columns for the logos with specified widths
+col1, col2, col3 = st.columns([3, 1, 1])
 
 # Adding padding by using empty space above the images
 padding = 45  # Adjust the amount of padding as needed
 
 with col1:
     st.write("\n" * padding)  # Adding space
-    st.image(logo1, use_column_width=False)
+    st.image(logo1, use_column_width=True)
 
 with col2:
-    st.image(logo2, use_column_width=False)
+    st.image(logo2, use_column_width=True)
 
 with col3:
     st.write("\n" * padding)  # Adding space
-    st.image(logo3, use_column_width=False)
+    st.image(logo3, use_column_width=True)
 
 st.title('Glossary Direktorat Jenderal Perbendaharaan')
 
