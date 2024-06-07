15cb87b385b7469721665a16a4cc25c77dd9d2c4
ameernoor <m.ameer.noor92@gmail.com>
2024-06-07 16:00:44 +0700

    testing image change

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ logo3 = os.path.join(BASE_DIR, 'images', 'intress.png')
 col1, col2, col3 = st.columns([8, 3, 4])
 
 # Adding padding by using empty space above the images
-padding = 50  # Adjust the amount of padding as needed
+padding1 = 70  # Adjust the amount of padding as needed
+padding2 = 150  # Adjust the amount of padding as needed
 
 with col1:
     st.write("\n" * padding)  # Adding space
@@ -27,7 +28,7 @@ with col2:
     st.image(logo2, use_column_width=True)
 
 with col3:
-    st.write("\n" * padding)  # Adding space
+    st.write("\n" * padding2)  # Adding space
     st.image(logo3, use_column_width=True)
 
 st.title('Glossary Direktorat Jenderal Perbendaharaan')
