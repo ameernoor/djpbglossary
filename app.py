51e47ec22654697bab8f44e0a95c1274bb04eddd
ameernoor <m.ameer.noor92@gmail.com>
2024-06-07 16:01:51 +0700

    testing minor change

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ logo3 = os.path.join(BASE_DIR, 'images', 'intress.png')
 col1, col2, col3 = st.columns([8, 3, 4])
 
 # Adding padding by using empty space above the images
-padding1 = 70  # Adjust the amount of padding as needed
-padding2 = 150  # Adjust the amount of padding as needed
+padding1 = 150  # Adjust the amount of padding as needed
+padding2 = 350  # Adjust the amount of padding as needed
 
 with col1:
     st.write("\n" * padding1)  # Adding space
