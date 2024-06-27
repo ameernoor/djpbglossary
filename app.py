087f55997035562842a4c897838fea933c5c39c1
ameernoor <m.ameer.noor92@gmail.com>
2024-06-27 12:50:29 +0700

    add wide config

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import os
 
+st.set_page_config(layout="wide")
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(BASE_DIR, "glossary_for_apps.xlsx")
 df = pd.read_excel(file_path, header=0)
