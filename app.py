3805d18379e524f1e6f2e022e207ec0034f1a965
ameernoor <m.ameer.noor92@gmail.com>
2024-06-27 11:57:10 +0700

    testing table display change

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ df.drop_duplicates(subset=['SINGKATAN', 'ISTILAH'], keep='first', inplace=True)
 # Function to generate an HTML table with text wrapping and handle NaN values
 def generate_html_table(data):
     # Replace NaN values with an empty string
-    data = data.fillna('')
+    data = data.fillna(' ')
     
     # Start the table with styles
     html = """
