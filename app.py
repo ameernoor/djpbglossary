8579f99ebde86fb14ebfd5086b196b8e5d113148
ameernoor <m.ameer.noor92@gmail.com>
2024-06-27 09:31:31 +0700

    testing table display change

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ st.markdown("""
 # Create columns for the logos with specified widths
 col1, col2, col3 = st.columns([8, 3, 4])
 with col1:
+    # Add spacer using custom CSS
     st.markdown('<div class="spacer"></div>', unsafe_allow_html=True)
     st.image(logo1, use_column_width=True)
 
@@ -32,7 +33,8 @@ with col2:
     st.image(logo2, use_column_width=True)
 
 with col3:
-    st.markdown('<div class="spacer" style="height: 40px;"></div>', unsafe_allow_html=True)
+    # Add larger spacer using custom CSS
+    st.markdown('<div class="spacer" style="height: 40px;"></div>', unsafe_allow_html=True)  # Adjust the height as needed
     st.image(logo3, use_column_width=True)
 
 st.title('Glossary Direktorat Jenderal Perbendaharaan')
@@ -42,6 +44,7 @@ query = st.text_input("Masukan Kata Kunci:", key="main_search")
 
 # Filtering data based on search query
 if query:
+    # Check if query is in any of the three columns
     mask = (
         df['SINGKATAN'].str.contains(query, case=False, na=False) |
         df['ISTILAH'].str.contains(query, case=False, na=False) |
@@ -49,12 +52,8 @@ if query:
     )
     results = df[mask]
     if not results.empty:
-        results = results.reset_index(drop=True)
-        st.dataframe(results, height=300)  # Use st.dataframe() for a scrollable table
-        # Expander for detailed text descriptions
-        with st.expander("See detailed descriptions"):
-            for index, row in results.iterrows():
-                st.text(f"{row['SINGKATAN']}: {row['ISTILAH']} - {row['BAHASA INGGRIS']}")
+        results = results.reset_index(drop=True)  # Reset index and do not keep the old one
+        st.table(results)  # Use st.table() which supports text wrapping
     else:
         st.write("Kata-Kata Tidak Dapat Ditemukan")
 
@@ -72,10 +71,6 @@ if selected_letter:
     filtered_data = df[df['SINGKATAN'].str.startswith(selected_letter, na=False)]
     if not filtered_data.empty:
         filtered_data = filtered_data.reset_index(drop=True)
-        st.dataframe(filtered_data, height=300)  # Adjust height as needed
-        # Expander for detailed text descriptions
-        with st.expander("See detailed descriptions"):
-            for index, row in filtered_data.iterrows():
-                st.text(f"{row['SINGKATAN']}: {row['ISTILAH']} - {row['BAHASA INGGRIS']}")
+        st.dataframe(filtered_data, width = 1000, height=300)  # Adjust height as needed
     else:
         st.write(f"Tidak ada istilah yang diawali huruf {selected_letter}")
